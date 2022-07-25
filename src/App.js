bbdba967754093a5953c95ba2dae1f429826f6cf
Fernando Torres <fernandorox1@hotmail.com>
2022-07-25 16:39:16 -0300

    Projeto final

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ const stages = [
   {id: 3, name: 'end'},
 ]
 
+const guessesQty = 3
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
@@ -26,59 +28,129 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("")
   const [letters, setLetters] = useState([])
 
-  const pickWordAndCategory = () => {
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState([guessesQty])
+  const [score, setScore] = useState(0)
+
+  const pickWordAndCategory = useCallback(() => {
     // Pick a random category
     const categories = Object.keys(words)
     const category = 
       categories[Math.floor(Math.random() * Object.keys(categories).length)]
 
-    console.log(category)
-
     // Pick a random word
     const word = 
       words[category][Math.floor(Math.random() * words[category].length)]
 
-    console.log(category)
-
     return{word, category}
-  }
+  }, [words])
 
   // Starts the game
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    // Clear all letters
+    clearLetterStates()
+
     // pick word and category
     const {word, category} = pickWordAndCategory()
 
     // Create an array of letters
     let wordLetters = word.toLowerCase().split("")
 
-    console.log(word, category)
-    console.log(wordLetters)
-
     // fill states
     setPickedWord(word)
     setPickedCategory(category)
-    setLetters(letters)
+    setLetters(wordLetters)
 
     setGameStage(stages[1].name)
-  }
+  }, [pickWordAndCategory])
 
   // Process
-  const verifyLetter = () => {
-    setGameStage(stages[2].name)
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase()
+
+    // Check if letter has already been utilized
+    if (
+      guessedLetters.includes(normalizedLetter) || 
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return
+    }
+
+    // Push guessed letter or remove a guess
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ])
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ])
+
+      setGuesses((actualGuesses) => actualGuesses - 1)
+    }
   }
+
+  const clearLetterStates = () => {
+    setGuessedLetters([])
+    setWrongLetters([])
+  }
+
+  useEffect(() => {
+    if (guesses <= 0) {
+      //reset all states
+      clearLetterStates()
+
+      setGameStage(stages[2].name)
+    }
+  }, [guesses])
+
+  // Check win condition
+  useEffect(() => {
+
+    const uniqueLetters = [...new Set(letters)]
+
+    // win condition
+    if(guessedLetters.length === uniqueLetters.length) {
+      // add score
+      setScore((actualScore) => actualScore += 100)
+
+      // restart game with new word
+      startGame()
+    }
+  }, [guessedLetters, letters, startGame])
+
+  console.log(guessedLetters)
+  console.log(wrongLetters)
 
   // Restart the game
   const retry = () => {
+    setScore(0)
+    setGuesses(guessesQty)
+
     setGameStage(stages[0].name)
   }
  
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame}/>}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
-      {gameStage === 'end' && <GameOver retry={retry}/>}
+      {gameStage === 'game' && (
+        <Game 
+          verifyLetter={verifyLetter} 
+          pickedWord={pickedWord} 
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
+      {gameStage === 'end' && <GameOver retry={retry} score={score}/>}
     </div>
-  );
+  )
 }
 
 export default App;
